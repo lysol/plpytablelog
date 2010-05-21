ebefc9d5e34dc8fbfc7a665aef3ef998a9b1f094
Derek Arnold <derek.arnold@dealerbuilt.com>
2010-05-21 09:22:30 -0500

    Added a script to build a complete script.
    I added a python script that will drill down through \i statements in
    psql-compatible sql scripts and build one large SQL script suitable for
    other interfaces to Postgres that do not support psql conventions.

---
+++ build_deployable_install.py
@@ -0,0 +1,17 @@
+out_lines = []
+
+def process_file(file_name):
+    """Function intended for recursive use to include \i files in psql
+    scripts."""
+    in_source = open(file_name, 'r')
+    for line in in_source.readlines():
+        if line.strip()[:2] == '\i':
+            next_file = line.strip()[3:].strip()
+            process_file(next_file)
+        else:
+            out_lines.append(line)
+
+source_file = 'logging.install.sql'
+process_file(source_file)
+
+print "\n".join(out_lines)
