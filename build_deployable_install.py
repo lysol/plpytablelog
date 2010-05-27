1b080eb83e65de2af6315462db1c2cbec1103aec
Derek Arnold <derek.arnold@dealerbuilt.com>
2010-05-27 17:41:51 -0500

    Fixed deploy script a little. Fixed get_record to handle DELETE events.

--- build_deployable_install.py
+++ build_deployable_install.py
@@ -1,45 +1,63 @@
-out_lines = []
 
-# Since Functions can't have SELECT statements that don't send results
-# somewhere, we have to replace them with PERFORM.
-perform_transforms = (
-    'SELECT logging.build_setup();',
-    'SELECT logging.deploy();',
-    )
+from optparse import OptionParser
 
-def process_file(file_name):
-    """Function intended for recursive use to include \i files in psql
-    scripts."""
-    in_source = open(file_name, 'r')
-    for line in in_source.readlines():
-        if line.strip()[:2] == '\i':
-            next_file = line.strip()[3:].strip()
-            process_file(next_file)
-        else:
-            appended = None
-            for trans in perform_transforms:
-                if trans in line:
-                    out_lines.append(line.replace('SELECT', 'PERFORM'))
-                    appended = True
-            if not appended:
-                out_lines.append(line)
 
-source_file = 'logging.install.sql'
-process_file(source_file)
+def main():
+    parser = OptionParser('usage: %prog inputfile')
+    parser.add_option('-e','--escaped', dest="escaped", 
+        action="store_true", default=False, help="Enable to escape the " + \
+        "output as if it was going into a non-dollar quoted field.")
+    (options, args) = parser.parse_args()
+    
+    out_lines = []
 
-text = "            ".join(out_lines)
+    # Since Functions can't have SELECT statements that don't send results
+    # somewhere, we have to replace them with PERFORM.
+    perform_transforms = (
+        'SELECT logging.build_setup();',
+        'SELECT logging.deploy();',
+        )
 
-text = """
-    CREATE OR REPLACE FUNCTION logging_install() RETURNS VOID AS $INSTALL$
-        BEGIN
-        --BEGIN
-""" + text + """
-        --EXCEPTION WHEN OTHERS THEN
-        --    RETURN;
-        --END;
-        END;
-        $INSTALL$ LANGUAGE plpgsql VOLATILE;
-        SELECT logging_install();
-"""
+    def process_file(file_name):
+        """Function intended for recursive use to include \i files in psql
+        scripts."""
+        in_source = open(file_name, 'r')
+        for line in in_source.readlines():
+            if line.strip()[:2] == '\i':
+                next_file = line.strip()[3:].strip()
+                process_file(next_file)
+            else:
+                appended = None
+                for trans in perform_transforms:
+                    if trans in line:
+                        out_lines.append(line.replace('SELECT', 'PERFORM'))
+                        appended = True
+                if not appended:
+                    out_lines.append(line)
+    if len(args) == 1:
+        source_file = args[0]
+    else:
+        source_file = 'logging.install.sql'
+    process_file(source_file)
 
-print text.replace("\n", "\\n").replace("'", "\\'")
+    text = "            ".join(out_lines)
+
+    text = """
+        CREATE OR REPLACE FUNCTION logging_install() RETURNS VOID AS $INSTALL$
+            BEGIN
+            --BEGIN
+    """ + text + """
+            --EXCEPTION WHEN OTHERS THEN
+            --    RETURN;
+            --END;
+            END;
+            $INSTALL$ LANGUAGE plpgsql VOLATILE;
+            SELECT logging_install();
+    """
+    if options.escaped:
+        print text.replace("\n", "\\n").replace("'", "\\'")
+    else:
+        print text
+
+if __name__ == "__main__":
+    main()
--- logging.get_record.sql
+++ logging.get_record.sql
@@ -2,6 +2,8 @@ CREATE OR REPLACE FUNCTION logging.get_record(log_table character varying, log_s
  RETURNS SETOF log_record
 AS $function$
 
+plpy.notice("Processing log seq %s", str(log_seq))
+
 return_results = []
 columns_processed = []
 
@@ -27,12 +29,14 @@ query_id = results[0]['query_id']
 date_modified = results[0]['date_modified']
 
 last_change_query = """
-    SELECT * FROM logging.sl
-    WHERE ROW(query_id, column_name)
+    SELECT * FROM logging.all_logs
+    WHERE log_table = $5
+        AND ROW(query_id, column_name)
         IN (
             SELECT  max(query_id) ,column_name
-            FROM logging.sl
-            WHERE query_id <= $4
+            FROM logging.all_logs
+            WHERE log_table = $5
+                AND query_id <= $4
             GROUP BY schema_name, table_name, record_seq, column_name
             HAVING schema_name = $1 AND table_name = $2 
                 AND record_seq = $3
@@ -41,17 +45,11 @@ last_change_query = """
     ORDER BY column_name, query_id;
 """
 
-current_row_query = """
-    SELECT * FROM "%s"."%s"
-    WHERE seq = $1
-""" % (schema_name, table_name)
 
-current_plan = plpy.prepare(current_row_query, ["int4"])
 last_change_plan = plpy.prepare(last_change_query,
-    ["text", "text", "int4", "int4"])
-current_row = plpy.execute(current_plan, [record_seq])
+    ["text", "text", "int4", "int4", "text"])
 last_change = plpy.execute(last_change_plan,
-    [schema_name, table_name, record_seq, query_id])
+    [schema_name, table_name, record_seq, query_id, log_table])
 
 for row in filter(lambda x: x['column_name'] != '', last_change):
     return_results.append({
@@ -70,21 +68,86 @@ for row in filter(lambda x: x['column_name'] != '', last_change):
 
     columns_processed.append(row['column_name'])
 
-columns = current_row[0].keys()
-for column in filter(lambda x: x not in columns_processed, columns):
-    return_results.append({
-        'seq': log_seq,
-        'column_name': column,
-        'column_value': current_row[0][column],
-        'modified_by': modified_by,
-        'client_addr': client_addr,
-        'query_id': query_id,
-        'date_modified': None,
-        'record_seq': record_seq,
-        'schema_name': schema_name,
-        'table_name': table_name,
-        'event': 'INSERT'
-    })
+
+is_deleted_query = """
+    SELECT event
+    FROM logging.all_logs
+    WHERE log_table = $1
+        AND seq = (
+            SELECT MAX(seq)
+            FROM logging.all_logs
+            WHERE log_table = $1
+                AND record_seq = $2
+            )
+    """
+is_deleted_plan = plpy.prepare(is_deleted_query, ["text", "int4"])
+
+event = plpy.execute(is_deleted_plan, [log_table, record_seq])[0]['event']
+if event == 'DELETE':
+    # Record was deleted. We will grab values from the DELETE record.
+    
+    final_change_query = """
+        SELECT * FROM logging.all_logs
+        WHERE log_table = $1
+            AND ROW(query_id, column_name)
+            IN (
+                SELECT max(query_id) ,column_name
+                FROM logging.all_logs
+                WHERE log_table = $1
+                    AND event = 'DELETE'
+                GROUP BY schema_name, table_name, record_seq, column_name
+                HAVING schema_name = $2 AND table_name = $3 
+                    AND record_seq = $4
+               )
+        ORDER BY column_name, query_id;
+        """
+    final_change_plan = plpy.prepare(final_change_query,
+        ["text", "text", "text", "int4"])
+    final_changes = plpy.execute(final_change_plan,
+        [log_table, schema_name, table_name, record_seq])
+
+    for row in filter(lambda x: x['column_name'] not in \
+        columns_processed, final_changes):
+        # Add it here
+        return_results.append({
+            'seq': row['seq'],
+            'column_name': row['column_name'],
+            'column_value': row['old_value'],
+            'modified_by': row['modified_by'],
+            'client_addr': row['client_addr'],
+            'query_id': row['query_id'],
+            'date_modified': row['date_modified'],
+            'record_seq': row['record_seq'],
+            'schema_name': row['schema_name'],
+            'table_name': row['table_name'],
+            'event': row['event']
+        })
+
+else:
+    # Record still exists. Pull values from that instead.
+
+    current_row_query = """
+        SELECT * FROM "%s"."%s"
+        WHERE seq = $1
+    """ % (schema_name, table_name)
+    current_plan = plpy.prepare(current_row_query, ["int4"])
+    current_row = plpy.execute(current_plan, [record_seq])
+
+    columns = current_row[0].keys()
+    for column in filter(lambda x: x not in columns_processed, columns):
+        return_results.append({
+            'seq': log_seq,
+            'column_name': column,
+            'column_value': current_row[0][column],
+            'modified_by': modified_by,
+            'client_addr': client_addr,
+            'query_id': query_id,
+            'date_modified': None,
+            'record_seq': record_seq,
+            'schema_name': schema_name,
+            'table_name': table_name,
+            'event': 'INSERT'
+        })
 
 return return_results
 
